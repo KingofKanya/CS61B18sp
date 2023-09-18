776a8372c3cd6d3153f36de4bcdef9a7d26e4a0a
红豆刨冰 <114462966+KingofKanya@users.noreply.github.com>
2023-09-18 21:33:55 +0800

    updata pro1a

--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
-public class ArrayDecueTest {
+public class ArrayDequeTest {
     @Test
     public void testaddsizeempty() {
         ArrayDeque<String> dq = new ArrayDeque<>();
