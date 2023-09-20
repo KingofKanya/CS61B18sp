0d15232b879f0d3a9c825360157612cc9ebc0f1a
红豆刨冰 <114462966+KingofKanya@users.noreply.github.com>
2023-09-20 17:30:48 +0800

    finish exam prep02

---
+++ disc/week2/QuickMath.java
@@ -0,0 +1,32 @@
+public class QuickMath {
+    public static void mulitplyBy3(int[] A){
+        for (int x : A) {
+            x = x * 3;
+        }
+    }
+
+    public static void mulitplyBy2(int[] A){
+        int[] B = A;
+        for(int i = 0;i < B.length;i++){
+            B[i] *= 2;
+        }
+    } 
+
+    public static void swap(int A, int B){
+        int temp = B;
+        B = A;
+        A = temp;
+    }
+    public static void main(String[] args) {
+        int[] arr;
+        arr = new int[]{2, 3, 3, 4};
+        mulitplyBy3(arr);
+
+        arr = new int[]{2, 3, 3, 4};
+        mulitplyBy2(arr);
+
+        int a = 6;
+        int b = 7;
+        swap(a, b);
+    }
+}
